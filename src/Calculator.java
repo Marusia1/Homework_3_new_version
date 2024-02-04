14c754506d8b95d433a2d8848d2ac2b96fa8ce2b
Marija <gestuverteja.marija@gmail.com>
2024-02-04 15:38:24 +0200

    3  homework new updated version

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,14 +1,14 @@
 public class Calculator {
-    int num1;
-    int num2;
-    double num3;
-    double num4;
-    public int addition(int num1, int num2) {
-        return num1 + num2;
+    int numb1;
+    int numb2;
+    double numb3;
+    double numb4;
+    public int addition(int numb1, int numb2) {
+        return numb1 + numb2;
 
     }
-    public boolean isGreater(double num3, double num4) {
-        return num3 >= num4;
+    public boolean isGreater(double numb3, double numb4) {
+        return numb3 >= numb4;
     }
 
 }
